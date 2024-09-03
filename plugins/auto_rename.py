3a1f9fcbbadd6ad4783becc6bc979a3cc0519f5f
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2024-09-03 18:49:22 +0530

    Update auto_rename.py

--- plugins/auto_rename.py
+++ plugins/auto_rename.py
@@ -12,7 +12,7 @@ async def auto_rename_command(client, message):
     # Save the format template to the database
     await AshutoshGoswami24.set_format_template(user_id, format_template)
 
-    await message.reply_text("**Auto Rename Format Updated Successfully! ✅**")
+    await message.reply_text("**𝐀𝐮𝐭𝐨 𝐑𝐞𝐧𝐚𝐦𝐞 𝐅𝐨𝐫𝐦𝐚𝐭 𝐔𝐩𝐝𝐚𝐭𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲! ✅**")
 
 @Client.on_message(filters.private & filters.command("setmedia"))
 async def set_media_command(client, message):
@@ -22,7 +22,7 @@ async def set_media_command(client, message):
     # Save the preferred media type to the database
     await AshutoshGoswami24.set_media_preference(user_id, media_type)
 
-    await message.reply_text(f"**Media Preference Set To :** {media_type} ✅")
+    await message.reply_text(f"**𝐌𝐞𝐝𝐢𝐚 𝐏𝐫𝐞𝐟𝐞𝐫𝐞𝐧𝐜𝐞 𝐒𝐞𝐭 𝐓𝐨 :** {media_type} ✅")
 
 
 
