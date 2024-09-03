8ce48291a1593571a2246c651b980c1607215932
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2024-09-03 18:51:49 +0530

    Update admin_panel.py

--- plugins/admin_panel.py
+++ plugins/admin_panel.py
@@ -47,7 +47,7 @@ async def tutorial(bot, message):
             ),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("⚡ Tutorial", url="https://t.me/AshuSupport")]]
+                [[InlineKeyboardButton("⚡ Tutorial", url="https://t.me/Yugen_Bots_Support")]]
             ),
         )
     except Exception as e:
