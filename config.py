1dd7c02a600aff1955302d48313defb17de10314
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2024-09-03 20:19:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config(object):
     # -- FORCE_SUB_CHANNELS = ["BotzPW","AshuSupport","AshutoshGoswami24"] -- # 
     FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', 'Yugen_Bots,Paradox_Bots').split(',')
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002197485186"))
-    PORT = int(os.environ.get("PORT", "8080"))
+    PORT = int(os.environ.get("PORT", ""))
     
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "False"))
